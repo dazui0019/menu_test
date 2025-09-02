b1379de3c64d9f388ad635b800445f385186b43d
dazui0019 <dazui0019@gmail.com>
2025-09-02 16:07:34 +0800

    feat(tim): add TIM10 interrupt handler for button debouncing
    - Enable TIM module and register callbacks in HAL config
    - Implement 5ms timer interrupt handler for button ticks
    - Reduce short press threshold from 300ms to 200ms
    - Add button event handling with single/double click detection
    - Initialize TIM10 with 5ms interval in main application

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -2,6 +2,14 @@
 #include "main.h"
 #include "stm32f4xx_hal_gpio.h"
 #include "stateMachine.h"
+#include "multi_button.h"
+#include "printf.h"
+#include "stm32f4xx_hal_tim.h"
+#include "tim.h"
+#include "time.h"
+#include "EventRecorder.h"
+
+void timer_5ms_interrupt_handler(TIM_HandleTypeDef *htim);
 
 /* Types of events */
 enum eventType {
@@ -37,10 +45,55 @@ static struct state menuGroupState = {
 //     .exitAction = &printExitMsg,
 // };
 
-void setup(){
+static Button btn0;
+
+uint8_t read_button_gpio(uint8_t button_id)
+{
+    switch (button_id) {
+        case 1:
+            return HAL_GPIO_ReadPin(BTN0_GPIO_Port, BTN0_Pin);
+        default:
+            return 0;
+    }
 }
 
-void loop(){
+void btn0_handler(Button* btn_handle)
+{
+    switch(btn_handle->event){
+        case BTN_SINGLE_CLICK:
+            printf("Button 0: Single Click\r\n");
+            break;
+        case BTN_DOUBLE_CLICK:
+            printf("Button 0: Double Click\r\n");
+            break;
+        default:
+            break;
+    }
+}
+
+void setup()
+{
+    EventRecorderInitialize (EventRecordAll, 1);
+    // 初始化按键 (active_level: 0=低电平有效, 1=高电平有效)
+    button_init(&btn0, read_button_gpio, 0, 1);
+    printf("Hello, World!\r\n");
+    button_attach(&btn0, BTN_SINGLE_CLICK, btn0_handler);
+    button_attach(&btn0, BTN_DOUBLE_CLICK, btn0_handler);
+
+    HAL_TIM_RegisterCallback(&htim10, HAL_TIM_PERIOD_ELAPSED_CB_ID, timer_5ms_interrupt_handler);
+
+    HAL_TIM_Base_Start_IT(&htim10);
+
+    button_start(&btn0);
+}
+
+void loop()
+{
     HAL_GPIO_TogglePin(LD0_GPIO_Port, LD0_Pin);
     HAL_Delay(100);
+}
+
+void timer_5ms_interrupt_handler(TIM_HandleTypeDef *htim)
+{
+    button_ticks();
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "spi.h"
+#include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -90,6 +91,7 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_USART1_UART_Init();
+  MX_TIM10_Init();
   /* USER CODE BEGIN 2 */
   setup();
   /* USER CODE END 2 */
