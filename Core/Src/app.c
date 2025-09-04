ecefc893763632f39584265048ed31f50ec6a1a4
dazui0019 <dazui0019@gmail.com>
2025-09-04 17:22:01 +0800

    feat(ui): integrate uGUI library for LCD display management
    - Add uGUI library files and configure build system
    - Implement UI initialization and state-dependent display updates
    - Reorganize state machine transitions with page actions
    - Update include paths and project configuration files

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -9,6 +9,7 @@
 #include "time.h"
 #include "EventRecorder.h"
 #include "st7735.h"
+#include "ugui.h"
 
 enum LCD_STATE{
     LCD_GROUP,
@@ -25,28 +26,36 @@ struct stateData{
     void* str;
 };
 
-struct stateMachine m;
-
 void timer_5ms_interrupt_handler(TIM_HandleTypeDef *htim);
+static bool compareButtonVal( void *btn_val, struct event *event );
 
+void ui_init();
+
+// stateMachine
+void settingAction( void *currentStateData, struct event *event, void *newStateData );
+void pageAction( void *currentStateData, struct event *event, void *newStateData );
 static void printEnterMsg( void *stateData, struct event *event );
 static void printExitMsg( void *stateData, struct event *event );
 static void printErrMsg( void *stateData, struct event *event );
 
 /* Types of events */
 enum eventType {
-   Event_button,
+    Event_lcd_init,
+    Event_button,
+    Event_refresh,
 };
 
-static bool compareButtonVal( void *btn_val, struct event *event );
+static Button btn0;
 
+struct stateMachine m;
 static struct state lcdGroupState, idleState, menuGroupState, selCurState, selDlyState, setCurState, setDlyState;
 
 static struct state lcdGroupState = {
    .parentState = NULL,
     .entryState = &idleState,
     .transitions = (struct transition[]){
-        { Event_button, NULL, NULL, NULL, &idleState},
+        { Event_lcd_init, NULL, NULL, pageAction, &idleState},
+        { Event_button, NULL, NULL, pageAction, &idleState},
     },
     .numTransitions = 1,
     .data = &(struct stateData){.num=LCD_GROUP, .str="lcd"},
@@ -58,20 +67,22 @@ static struct state idleState = {
     .parentState = &lcdGroupState,
     .entryState = NULL,
     .transitions = (struct transition[]){
-        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, NULL, &selCurState },
+        { Event_lcd_init, NULL, NULL, pageAction, &idleState},
+        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, pageAction, &selCurState },
+        {Event_button, NULL, NULL, NULL, &idleState}
     },
-    .numTransitions = 1,
+    .numTransitions = 3,
     .data = &(struct stateData){.num=LCD_IDLE, .str="idle"},
     .entryAction = printEnterMsg,
     .exitAction = printExitMsg,
 };
 
-void settingAction( void *currentStateData, struct event *event, void *newStateData );
 static struct state menuGroupState = {
     .parentState = &lcdGroupState,
     .entryState = NULL,
     .transitions = (struct transition[]){
-        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, NULL, &selCurState },
+        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, pageAction, &selCurState },
+        {Event_button, NULL, NULL, NULL, &menuGroupState},
     },
     .numTransitions = 2,
     .data = &(struct stateData){.num=LCD_MENU, .str="menu"},
@@ -83,9 +94,9 @@ static struct state selCurState = {
     .parentState = &menuGroupState,
     .entryState = NULL,
     .transitions = (struct transition[]){
-        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, NULL, &idleState },
-        { Event_button, (void *)BTN_SINGLE_CLICK, compareButtonVal, NULL, &selDlyState },
-        { Event_button, (void *)BTN_DOUBLE_CLICK, compareButtonVal, NULL, &setCurState },
+        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, pageAction, &idleState },
+        { Event_button, (void *)BTN_SINGLE_CLICK, compareButtonVal, pageAction, &selDlyState },
+        { Event_button, (void *)BTN_DOUBLE_CLICK, compareButtonVal, pageAction, &setCurState },
         { Event_button, NULL, NULL, NULL, &selCurState },
     },
     .numTransitions = 4,
@@ -98,9 +109,9 @@ static struct state selDlyState = {
     .parentState = &menuGroupState,
     .entryState = NULL,
     .transitions = (struct transition[]){
-        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, NULL, &idleState },
-        { Event_button, (void *)BTN_SINGLE_CLICK, compareButtonVal, NULL, &selCurState },
-        { Event_button, (void *)BTN_DOUBLE_CLICK, compareButtonVal, NULL, &setDlyState },
+        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, pageAction, &idleState },
+        { Event_button, (void *)BTN_SINGLE_CLICK, compareButtonVal, pageAction, &selCurState },
+        { Event_button, (void *)BTN_DOUBLE_CLICK, compareButtonVal, pageAction, &setDlyState },
         { Event_button, NULL, NULL, NULL, &selDlyState },
     },
     .numTransitions = 4,
@@ -113,7 +124,7 @@ static struct state setCurState = {
     .parentState = &menuGroupState,
     .entryState = NULL,
     .transitions = (struct transition[]){
-        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, NULL, &selCurState },
+        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, pageAction, &selCurState },
         { Event_button, (void *)BTN_SINGLE_CLICK, compareButtonVal, settingAction, &setCurState },
         { Event_button, (void *)BTN_DOUBLE_CLICK, compareButtonVal, NULL, &setCurState },
         { Event_button, NULL, NULL, NULL, &setCurState },
@@ -128,7 +139,7 @@ static struct state setDlyState = {
     .parentState = &menuGroupState,
     .entryState = NULL,
     .transitions = (struct transition[]){
-        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, NULL, &selDlyState },
+        { Event_button, (void *)BTN_LONG_PRESS_START, compareButtonVal, pageAction, &selDlyState },
         { Event_button, (void *)BTN_SINGLE_CLICK, compareButtonVal, settingAction, &setDlyState },
         { Event_button, (void *)BTN_DOUBLE_CLICK, compareButtonVal, NULL, &setDlyState },
         { Event_button, NULL, NULL, NULL, &setDlyState }, 
@@ -142,8 +153,6 @@ static struct state setDlyState = {
 static struct state errorState = {
    .entryAction = &printErrMsg
 };
-
-static Button btn0;
 
 uint8_t read_button_gpio(uint8_t button_id)
 {
@@ -188,7 +197,13 @@ void btn0_handler(Button* btn_handle)
 
 void setup()
 {
+    ST7735_Init();
+    ST7735_FillScreen(ST7735_WHITE);
+    ST7735_BLK_CTRL(1);
+    ui_init();
+
     stateM_init( &m, &idleState, &errorState );
+    stateM_handleEvent( &m, &(struct event){ Event_lcd_init, NULL } );
 
     EventRecorderInitialize (EventRecordAll, 1);
     // 初始化按键 (active_level: 0=低电平有效, 1=高电平有效)
@@ -204,10 +219,6 @@ void setup()
     HAL_TIM_Base_Start_IT(&htim10);
 
     button_start(&btn0);
-
-    ST7735_Init();
-    ST7735_FillScreen(ST7735_WHITE);
-    ST7735_BLK_CTRL(1);
 }
 
 void loop()
@@ -229,6 +240,32 @@ static bool compareButtonVal( void *btn_val, struct event *event )
    return (intptr_t)btn_val == (intptr_t)event->data;
 }
 
+// 页面切换动作
+void pageAction( void *currentStateData, struct event *event, void *newStateData )
+{
+    printf("%s --> %s\r\n", (char *)(((struct stateData*)currentStateData)->str), (char *)(((struct stateData*)newStateData)->str));
+    switch((uint32_t)(((struct stateData*)newStateData)->num)){
+        case LCD_IDLE:
+            UG_PutString(0, 0, "IDLE  ");
+            break;
+        case LCD_CUR_SEL:
+            UG_PutString(0, 0, "CUR SEL");
+            break;
+        case LCD_DLY_SEL:
+            UG_PutString(0, 0, "DLY SEL");
+            break;
+        case LCD_CUR_SET:
+            UG_PutString(0, 0, "CUR SET");
+            break;
+        case LCD_DLY_SET:
+            UG_PutString(0, 0, "DLY SET");
+            break;
+        default:
+            break;
+    }
+}
+
+// 设置动作
 void settingAction( void *currentStateData, struct event *event, void *newStateData )
 {
     printf("%s --> %s\r\n", (char *)(((struct stateData*)currentStateData)->str), (char *)(((struct stateData*)newStateData)->str));
@@ -249,6 +286,26 @@ void settingAction( void *currentStateData, struct event *event, void *newStateD
     }
 }
 
+void refreshAction(void *currentStateData, struct event *event, void *newStateData)
+{
+    if(((struct stateData* )currentStateData)->num != ((struct stateData* )newStateData)->num)
+        return;
+    switch (((struct stateData* )newStateData)->num) {
+        case LCD_IDLE:
+            break;
+        case LCD_CUR_SEL:
+            break;
+        case LCD_DLY_SEL:
+            break;
+        case LCD_CUR_SET:
+            break;
+        case LCD_DLY_SET:
+            break;
+        default:
+            break;
+    }
+}
+
 static void printErrMsg( void *stateData, struct event *event )
 {
    printf( "ENTERED ERROR STATE!\n" );
@@ -264,4 +321,14 @@ static void printExitMsg( void *stateData, struct event *event )
 {
     struct stateData* data = (struct stateData*)stateData;
     printf( "Exiting %s state\n", (char *)data->str );
+}
+
+UG_GUI gui;
+#define UI_FONT FONT_8X14
+void ui_init()
+{
+    UG_Init(&gui, ST7735_DrawPixel, 128, 160);
+    UG_FontSelect(&FONT_8X14);
+    UG_DriverRegister(DRIVER_FILL_FRAME, ST7789_FillRectangle_XY);
+    UG_FillScreen(C_BLACK);
 }
